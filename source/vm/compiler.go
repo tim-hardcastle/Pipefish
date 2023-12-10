c69ea7d4aa90d7a55c6e8466aa10247af05d678d
Tim Hardcastle <timphardcastle@gmail.com>
2023-12-10 01:01:13 -0800

    Clean up

--- source/vm/compiler.go
+++ source/vm/compiler.go
@@ -40,61 +40,41 @@ func (cp *Compiler) Compile(source, sourcecode string) {
 	print("\nCompiling :\n\n")
 	cp.vm = blankVm()
 	node := cp.p.ParseLine(source, sourcecode)
-	cp.compileNode(node, true)
+	cp.compileNode(node)
 	cp.emit(ret)
 	cp.vm.mem = make([]Value, cp.memTop)
 }
 
-// When we want to return froma block of bytecode we want the result to be stashed in m0,
-// and then we want a rtn statement. Whereas otherwise we mant the result appended to the end
-// of memory.
-//
-// To know when we have in fact finished, we pass the 'dest' flag along with the node to be
-// compiled. This tells it to use the first of these options. So 'dest' is set when we run 'main', or
-// a line from the REPL, or whatever. We usually then want to unset it when making a recusive call to
-// compileNode, except in the case where the node we're compiling is an 'if' branch, where we can and
-// should induce an early return by this means.
-//
-// We have two different ways of emiting an opcode:
-//
-//   (1) 'emit' is for opcodes without a destination memory address at all, e.g. jumps, returns,
-//       flow-of-control generally.
-//
-//   (2) 'put' takes the value of 'dest' as its second parameter (after the opcode) and emits code
-//       accordingly.
-//
+// We have two different ways of emiting an opcode: 'emit' does it the regular way, 'put' ensures that
+// the destination is the most recent memory address.
 
-func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
-	// var offset uint32
-	// if dest {
-	// 	offset = 1
-	// }
+func (cp *Compiler) compileNode(node ast.Node) typeList {
 	switch node := node.(type) {
 	case *ast.IntegerLiteral:
-		cp.put(asgc, dest, cp.constantTop)
+		cp.put(asgc, cp.constantTop)
 		cp.addConstant(INT, node.Value)
 		return []valType{&simpleType{t: INT}}
 	case *ast.StringLiteral:
-		cp.put(asgc, dest, cp.constantTop)
+		cp.put(asgc, cp.constantTop)
 		cp.addConstant(STRING, node.Value)
 		return []valType{&simpleType{t: STRING}}
 	case *ast.BooleanLiteral:
-		cp.put(asgc, dest, cp.constantTop)
+		cp.put(asgc, cp.constantTop)
 		cp.addConstant(BOOL, node.Value)
 		return []valType{&simpleType{t: BOOL}}
 	case *ast.FloatLiteral:
-		cp.put(asgc, dest, cp.constantTop)
+		cp.put(asgc, cp.constantTop)
 		cp.addConstant(FLOAT, node.Value)
 		return []valType{&simpleType{t: FLOAT}}
 	case *ast.InfixExpression:
 		if node.Operator == "==" {
-			lTypes := cp.compileNode(node.Args[0], false)
+			lTypes := cp.compileNode(node.Args[0])
 			if lTypes.only(ERROR) {
 				cp.p.Throw("comp/eq/err/a", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
 			}
 			leftRg := cp.memTop - 1
-			rTypes := cp.compileNode(node.Args[2], false)
+			rTypes := cp.compileNode(node.Args[2])
 			if rTypes.only(ERROR) {
 				cp.p.Throw("comp/eq/err/b", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
@@ -112,13 +92,13 @@ func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
 			if len(oL) == 1 && len(lTypes) == 1 && len(rTypes) == 1 {
 				switch oL[0].concreteType() {
 				case INT:
-					cp.put(equi, dest, leftRg, rightRg)
+					cp.put(equi, leftRg, rightRg)
 				case STRING:
-					cp.put(equs, dest, leftRg, rightRg)
+					cp.put(equs, leftRg, rightRg)
 				case BOOL:
-					cp.put(equb, dest, leftRg, rightRg)
+					cp.put(equb, leftRg, rightRg)
 				case FLOAT:
-					cp.put(equf, dest, leftRg, rightRg)
+					cp.put(equf, leftRg, rightRg)
 				default:
 					panic("Unimplemented comparison type.")
 				}
@@ -131,7 +111,7 @@ func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
 		}
 	case *ast.LazyInfixExpression:
 		if node.Operator == "or" {
-			lTypes := cp.compileNode(node.Left, false)
+			lTypes := cp.compileNode(node.Left)
 			if !lTypes.contains(BOOL) {
 				cp.p.Throw("comp/or/bool/left", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
@@ -140,18 +120,18 @@ func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
 			cp.emit(qtru, leftRg, cp.codeTop+2)
 			backtrack := cp.codeTop
 			cp.emit(jmp, MAX_32)
-			rTypes := cp.compileNode(node.Right, false)
+			rTypes := cp.compileNode(node.Right)
 			if !rTypes.contains(BOOL) {
 				cp.p.Throw("comp/or/bool/right", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
 			}
 			rightRg := cp.memTop - 1
 			cp.vm.code[backtrack].args[0] = cp.codeTop
-			cp.put(orb, dest, leftRg, rightRg)
+			cp.put(orb, leftRg, rightRg)
 			return []valType{&simpleType{t: BOOL}}
 		}
 		if node.Operator == "and" {
-			lTypes := cp.compileNode(node.Left, false)
+			lTypes := cp.compileNode(node.Left)
 			if !lTypes.contains(BOOL) {
 				cp.p.Throw("comp/and/bool/left", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
@@ -159,21 +139,21 @@ func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
 			leftRg := cp.memTop - 1
 			backtrack := cp.codeTop
 			cp.emit(qtru, leftRg, MAX_32)
-			rTypes := cp.compileNode(node.Right, false)
+			rTypes := cp.compileNode(node.Right)
 			if !rTypes.contains(BOOL) {
 				cp.p.Throw("comp/and/bool/right", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
 			}
 			rightRg := cp.memTop - 1
 			cp.vm.code[backtrack].args[1] = cp.codeTop
-			cp.put(andb, dest, leftRg, rightRg)
+			cp.put(andb, leftRg, rightRg)
 			return []valType{&simpleType{t: BOOL}}
 		}
 		if node.Operator == ":" {
 			if node.Left.GetToken().Type == token.ELSE {
-				return cp.compileNode(node.Right, false)
+				return cp.compileNode(node.Right)
 			}
-			lTypes := cp.compileNode(node.Left, false)
+			lTypes := cp.compileNode(node.Left)
 			if !lTypes.contains(BOOL) {
 				cp.p.Throw("comp/cond/bool", node.Token)
 				return []valType{&simpleType{t: TYPE_ERROR}}
@@ -181,24 +161,24 @@ func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
 			leftRg := cp.memTop - 1
 			backtrack := cp.codeTop
 			cp.emit(qtru, leftRg, MAX_32)
-			rTypes := cp.compileNode(node.Right, false)
-			cp.put(asgm, dest, cp.memTop-1)
+			rTypes := cp.compileNode(node.Right)
+			cp.put(asgm, cp.memTop-1)
 			cp.emit(jmp, cp.memTop+2)
 			cp.vm.code[backtrack].args[1] = cp.codeTop
-			cp.reput(asgc, dest, C_U_OBJ)
+			cp.reput(asgc, C_U_OBJ)
 			return rTypes.union([]valType{&simpleType{t: UNSAT}})
 		}
 		if node.Operator == ";" {
-			lTypes := cp.compileNode(node.Left, false)
+			lTypes := cp.compileNode(node.Left)
 			leftRg := cp.memTop - 1
 			backtrack := cp.codeTop
 			cp.emit(qtyp, leftRg, UNSAT, MAX_32)
-			rTypes := cp.compileNode(node.Right, false)
+			rTypes := cp.compileNode(node.Right)
 			rightRg := cp.memTop - 1
-			cp.put(asgm, dest, rightRg)
+			cp.put(asgm, rightRg)
 			cp.emit(jmp, cp.codeTop+2)
 			cp.vm.code[backtrack].args[2] = cp.codeTop
-			cp.reput(asgm, dest, leftRg)
+			cp.reput(asgm, leftRg)
 			if !(lTypes.contains(UNSAT) && rTypes.contains(UNSAT)) {
 				return lTypes.union(rTypes).without(&simpleType{UNSAT})
 			}
@@ -207,9 +187,9 @@ func (cp *Compiler) compileNode(node ast.Node, dest bool) typeList {
 		panic("Unimplemented lazy infix.")
 	case *ast.PrefixExpression:
 		if node.Operator == "not" {
-			allTypes := cp.compileNode(node.Args[0], false)
+			allTypes := cp.compileNode(node.Args[0])
 			if allTypes.only(BOOL) {
-				cp.put(notb, dest, cp.memTop-1)
+				cp.put(notb, cp.memTop-1)
 				return []valType{&simpleType{t: BOOL}}
 			}
 			if !allTypes.contains(BOOL) {
@@ -236,15 +216,15 @@ func (cp *Compiler) addVariable(env *environment, name string, val Value, acc va
 	cp.memTop++
 }
 
-func (cp *Compiler) put(opcode opcode, dest bool, args ...uint32) {
+func (cp *Compiler) put(opcode opcode, args ...uint32) {
 	args = append([]uint32{cp.memTop}, args...)
 	cp.emit(opcode, args...)
 	cp.memTop++
 }
 
-func (cp *Compiler) reput(opcode opcode, dest bool, args ...uint32) {
+func (cp *Compiler) reput(opcode opcode, args ...uint32) {
 	cp.memTop--
-	cp.put(opcode, dest, args...)
+	cp.put(opcode, args...)
 }
 
 func (cp *Compiler) emit(opcode opcode, args ...uint32) {
