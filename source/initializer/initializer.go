57cd6048f318c66599908062b61a4368423e193d
Peachpit <peachpit.site@gmail.com>
2024-12-06 01:18:24 -0800

    Bugfix

--- source/initializer/initializer.go
+++ source/initializer/initializer.go
@@ -2343,7 +2343,7 @@ func MakeFilepath(scriptFilepath string) string {
 	if len(scriptFilepath) >= 4 && scriptFilepath[0:4] == "hub/" {
 		doctoredFilepath = filepath.Join(settings.PipefishHomeDirectory, filepath.FromSlash(scriptFilepath))
 	}
-	if len(scriptFilepath) >= 4 && scriptFilepath[0:4] == "rsc-pf/" {
+	if len(scriptFilepath) >= 7 && scriptFilepath[0:7] == "rsc-pf/" {
 		doctoredFilepath = filepath.Join(settings.PipefishHomeDirectory, "source", "initializer", filepath.FromSlash(scriptFilepath))
 	}
 	if settings.StandardLibraries.Contains(scriptFilepath) {
