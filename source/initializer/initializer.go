8574ae0a67e5ffed14e88b0a0359568b403cd096
Peachpit <peachpit.site@gmail.com>
2024-12-10 06:05:28 -0800

    Experimentally disable one continue statement

--- source/initializer/initializer.go
+++ source/initializer/initializer.go
@@ -579,7 +579,7 @@ func (iz *initializer) InitializeNamespacedImportsAndReturnUnnamespacedImports()
 		}
 		if namespace == "" {
 			unnamespacedImports = append(unnamespacedImports, scriptFilepath)
-			continue
+			//continue
 		}
 		newIz := NewInitializer()
 		newIz.Common = iz.Common
