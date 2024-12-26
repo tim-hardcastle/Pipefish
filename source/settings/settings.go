fec859e8690ebf558696b178de1cb10963a9ab41
Peachpit <peachpit.site@gmail.com>
2024-12-25 16:12:42 -0800

    Turn instrumentation on

--- source/settings/settings.go
+++ source/settings/settings.go
@@ -23,7 +23,7 @@ func MandatoryImportSet() dtypes.Set[string] {
 
 var ThingsToIgnore = (dtypes.MakeFromSlice(MandatoryImports)).Add("rsc-pf/hub.pf").Add("Builtin constant").Add("rsc/worldlite.pf")
 
-// This is replicated in the hub and any changes made here must be reflected there.
+// This is replicated in the hub and any changes made here must be reflected there. TODO --- don't.
 var StandardLibraries = dtypes.MakeFromSlice([]string{"path/filepath", "fmt", "math", "path", "regexp", "strings", "time", "unicode"})
 
 const (
@@ -42,7 +42,7 @@ const (
 	SHOW_RUNTIME           = false // Note that this will show the hub's runtime too at present 'cos it can't tell the difference. TODO.
 	SHOW_RUNTIME_VALUES    = false // Shows the contents of memory locations on the rhs of anything (i.e. not the dest).
 	SHOW_XCALLS            = false
-	SHOW_GOLANG            = false
+	SHOW_GOLANG            = true
 
 	SHOW_TESTS = false // Says whether the tests should say what is being tested, useful if one of them crashes and we don't know which.
 )
