caa3578646a1177d2fd3e232ed1c07c3bd160ed1
Peachpit <peachpit.site@gmail.com>
2025-01-09 15:53:09 -0800

    Separate tracking out of main vm.go file

--- source/vm/vm.go
+++ source/vm/vm.go
@@ -1,7 +1,6 @@
 package vm
 
 import (
-	"bytes"
 	"database/sql"
 	"fmt"
 	"html/template"
@@ -105,31 +104,6 @@ const (
 	SQL_SNIPPET
 	HTML_SNIPPET
 )
-
-// This contains the information we need to generate tracking reports at runtime.
-type TrackingData struct {
-	Flavor TrackingFlavor
-	Tok    *token.Token
-	Args   []any
-}
-
-type TrackingFlavor int
-
-const (
-	TR_CONDITION TrackingFlavor = iota
-	TR_ELSE
-	TR_FNCALL
-	TR_LITERAL
-	TR_RESULT
-	TR_RETURN
-)
-
-func (vm *Vm) trackingIs(i int, tf TrackingFlavor) bool {
-	if i < 0 || i >= len(vm.LiveTracking) {
-		return false
-	}
-	return vm.LiveTracking[i].Flavor == tf
-}
 
 // Container for the data we push when a function might be about to do recursion.
 type recursionData struct {
@@ -2061,83 +2035,7 @@ func (vm *Vm) NewIterator(container values.Value, keysOnly bool, tokLoc uint32) 
 	}
 }
 
-func (vm *Vm) TrackingToString() string {
-	if len(vm.LiveTracking) == 0 {
-		return ("\nNo tracking data exists.\n")
-	}
-	var out bytes.Buffer
-	out.WriteString("\n")
-	for i, td := range vm.LiveTracking {
-		args := td.Args
-		switch td.Flavor {
-		case TR_CONDITION:
-			out.WriteString("At@line ")
-			out.WriteString(strconv.Itoa(td.Tok.Line))
-			out.WriteString("@we evaluated the condition ")
-			out.WriteString(text.Emph(args[0].(string)))
-			out.WriteString(". ")
-		case TR_ELSE:
-			out.WriteString("At@line ")
-			out.WriteString(strconv.Itoa(td.Tok.Line))
-			out.WriteString("@we took the ")
-			out.WriteString(text.Emph("else"))
-			out.WriteString(" branch")
-			if !vm.trackingIs(i+1, TR_RETURN) {
-				out.WriteString(".\n")
-			}
-		case TR_FNCALL:
-			out.WriteString("We called function ")
-			out.WriteString(text.Emph(args[0].(string)))
-			out.WriteString(" — defined at@line ")
-			out.WriteString(strconv.Itoa(td.Tok.Line))
-			out.WriteString("@")
-			if len(args) > 1 {
-				out.WriteString("— with ")
-				sep := ""
-				for i := 1; i < len(args); i = i + 2 {
-					out.WriteString(sep)
-					out.WriteString(text.Emph(args[i].(string) + " = " + vm.Literal(args[i+1].(values.Value))))
-					sep = ", "
-				}
-			}
-			out.WriteString(".\n")
-		case TR_LITERAL:
-			out.WriteString("Log at@line ")
-			out.WriteString(strconv.Itoa(td.Tok.Line))
-			out.WriteString("@: ")
-			out.WriteString(args[0].(values.Value).V.(string))
-			out.WriteString("\n")
-		case TR_RESULT:
-			if args[0].(values.Value).V.(bool) {
-				out.WriteString("The condition succeeded.\n")
-			} else {
-				out.WriteString("The condition failed.\n")
-			}
-		case TR_RETURN:
-			if args[1].(values.Value).T != values.UNSATISFIED_CONDITIONAL {
-				if vm.trackingIs(i-1, TR_ELSE) {
-					out.WriteString(", so at")
-				} else {
-					out.WriteString("At")
-				}
-				out.WriteString("@line ")
-				out.WriteString(strconv.Itoa(td.Tok.Line))
-				out.WriteString("@")
-				//out.WriteString("of ")
-				//out.WriteString(text.Emph(td.tok.Source))
-				//out.WriteString(" ")
-				out.WriteString("function ")
-				out.WriteString(text.Emph(args[0].(string)))
-				out.WriteString(" returned ")
-				out.WriteString(vm.Literal(args[1].(values.Value)))
-				out.WriteString(".\n")
-			}
-		}
-
-	}
-	return out.String()
-}
-
+// Constants for describing the syntax of functions.
 const (
 	PREFIX uint32 = iota
 	INFIX
