a86ff7d8a70f88e7c8b6ef1333ad47a3ff343263
Peachpit <peachpit.site@gmail.com>
2025-02-16 00:51:30 -0800

    Add instrumentation

--- source/compiler/compfcall.go
+++ source/compiler/compfcall.go
@@ -658,8 +658,8 @@ func (cp *Compiler) seekFunctionCall(b *bindle) AlternateType {
 				if F.HasGo {
 					cp.cmP("Emitting Go function call.", b.tok)
 					convErrorLoc := cp.reserveError("golang/conv", b.tok)
-					println("Calling Go function number", F.GoNumber)
-					println("Function parameters are", F.HasGo, F.HiReg, F.LoReg, F.OutReg)
+					println("Compiling call of function number", F.GoNumber)
+					println("Function parameters are", F.HasGo, F.LoReg, F.HiReg, F.OutReg, len(b.valLocs))
 					args := append([]uint32{b.outLoc, convErrorLoc, F.GoNumber}, b.valLocs...)
 					cp.Emit(vm.Gofn, args...)
 					if len(branch.Node.Fn.NameRets) == 0 {
